ad8f5d54c13ba5735a71e912a3667ae090b5b99e
Elena <Eva71879@gmail.com>
2023-12-01 21:16:38 +0300

    changed comments file

--- assets/script/alphabet_script.js
+++ assets/script/alphabet_script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'];
+const alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 const randomLetter1 = Math.floor(Math.random() * alphabet.length);
 const randomLetter2 = Math.floor(Math.random() * alphabet.length);
 const randomLetter3 = Math.floor(Math.random() * alphabet.length);
@@ -9,6 +9,20 @@ const randomLetter4 = Math.floor(Math.random() * alphabet.length);
 const randomWord = alphabet[randomLetter1] + alphabet[randomLetter2] + alphabet[randomLetter3] + alphabet[randomLetter4];
 console.log(randomWord);
 document.writeln(randomWord);
+
+// вариант Кота
+
+// const wordLength = prompt("Сколько символов должно быть в слове") //как алерт для пользователя
+// const alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+// let result = "";
+// const randomWord = () => {
+//  for (let i = 0; i < wordLength; i++) {
+//   const randomIndex = Math.floor(Math.random() * alphabet.length);
+//   result += alphabet[randomIndex];
+// }
+// }
+// randomWord()
+// document.body.innerText = result;
 
 // 3. Создать программу, которая генерирует случайное слово из четырёх букв.
     
--- assets/script/comments.js
+++ assets/script/comments.js
@@ -4,6 +4,8 @@ const inputName = document.querySelector ('.inputName');
 const outputName = document.querySelector ('.aside__outputName');
 const name = inputName.value;
 
+// - под заголовком «Оставьте ваш комментарий» должен быть чекбокс, который даёт выбор показывать ваше имя в комментарии или нет;
+
    function postName () {
     if (inputName.value.trim().length) {
         outputName.innerText = inputName.value;
@@ -11,23 +13,56 @@ const name = inputName.value;
         outputName.innerHTML = outputName.innerText.toLowerCase();
         outputName.innerHTML = outputName.innerText.charAt(0).toUpperCase() + outputName.innerText.slice(1);
     }
+    else if (inputName.value === '') {
+        outputName.innerText = "Username";
+    }
     }
 
     function postAvatar () {
         const inputAvatar = document.querySelector ('.inputAvatar');
         const outputAvatar = document.querySelector ('.aside__outputAvatar');
-        if (inputAvatar.value.trim().length) {    
-        outputAvatar.src = inputAvatar.value;
-        inputAvatar.value = '';
-    }
+        
+        const avatar1 = 'assets/icons/chicken-svgrepo-com.svg';
+        const avatar2 = 'assets/icons/hedgehog-svgrepo-com.svg';
+        const avatar3 = 'assets/icons/keroppi-svgrepo-com.svg';
+        const avatar4 = 'assets/icons/mouse-svgrepo-com.svg';
+        const avatar5 = 'assets/icons/zebra-svgrepo-com.svg';
+        const allAvatars = [avatar1, avatar2, avatar3, avatar4, avatar5];
+        const randomAvatar = Math.floor(Math.random() * allAvatars.length);
+
+        if (inputAvatar.value === '') {
+            outputAvatar.style.opacity = 1;
+            outputAvatar.src = allAvatars[randomAvatar];  
+        }
+
+        else if (inputAvatar.value.trim().length) {    
+            outputAvatar.src = inputAvatar.value;
+            inputAvatar.value = '';
 }
+    }
+
+    const outputComment = document.querySelector ('.aside__outputComment');
 
     function postComment () {
         const inputComment = document.querySelector ('.main__inputComment');
-        const outputComment = document.querySelector ('.aside__outputComment');
+        
+        const commentTime = document.querySelector ('.aside__commentTime');
+
+        const options = {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        timezone: 'UTC',
+    };
+    const setCommentDate = new Date().toLocaleString("ru", options);
+    const now = new Date();
+    const setCommentTime = now.toLocaleTimeString();
+    const setCommentDateAndTime = `Комментарий добавлен ${setCommentDate} в ${setCommentTime}`;
+
         if (inputComment.value.trim().length) {
         outputComment.innerText = inputComment.value;
-        inputComment.value = '';        
+        inputComment.value = '';
+        commentTime.innerText = setCommentDateAndTime;
     }
 }
 
@@ -60,3 +95,8 @@ buttonPostAll.addEventListener('click', postAll);
 // const comment1 = checkSpam('buy ViAgRA now'); //результат buy *** now
 // const comment2 = checkSpam('free xxx'); //результат free ***
 // const comment3 = checkSpam("innocent rabbit") ; //результат innocent rabbit
+
+// - напротив аватара и имени должна появляться дата когда и во сколько был написан комментарий;
+// - под заголовком «Оставьте ваш комментарий» должен быть чекбокс, который даёт выбор показывать ваше имя в комментарии или нет;
+// - если пользователь не указал имя, то вместо имени должно появляться `username` ;
+// - если пользователь не ввел ссылку на аватар, то должна появляться стандартная аватарка. Стандартных аватаров должно быть больше пяти, они должны подставляться в рандомном порядке.
